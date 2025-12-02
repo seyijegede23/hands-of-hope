a7f2ac1b30ce586f166eae45d21491331789eb12
seyijegede23 <63824763+seyijegede23@users.noreply.github.com>
2025-12-02 01:13:51 +0100

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -42,7 +42,7 @@ const Donate = () => {
 
     try {
       // Call the backend server
-      const response = await fetch("https://heartofhopeserver1.onrender.com/create-checkout-session", {
+      const response = await fetch("https://heartofhopeserver1.vercel.app/create-checkout-session", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
